2a4a7adf9a4b40d0b65d8d10dac28885767bafd7
Joseph Jiminian <jj3945@nyu.edu>
2026-02-25 18:31:23 +0100

    updated landing page and visualization tab

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ import sklearn
 
 from ydata_profiling import ProfileReport
 from streamlit.components.v1 import html
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
 # from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(
@@ -20,12 +25,12 @@ st.set_page_config(
 
 ## Step 01 - Setup
 st.sidebar.title("Healthcare Clinics 🏥")
-page = st.sidebar.selectbox("Select Page",["Introduction 📘","Visualization 📊", "Automated Report 📑","Prediction"])
+page = st.sidebar.selectbox("Select Page",["Business Case 📘","Visualization 📊", "Automated Report 📄", "Prediction"])
 
 
 #st.video("video.mp4")
 
-st.image("https://media.istockphoto.com/id/1524913019/fr/vectoriel/illustration-vectorielle-de-b%C3%A2timent-hospitalier-dans-un-design-plat.jpg?s=612x612&w=0&k=20&c=-hJTmokrmJ2nnmJoANeSkAAwmyrScKZp75jD6D8OIII=")
+st.image("hospital.jpg")
 
 st.write("   ")
 st.write("   ")
@@ -34,15 +39,35 @@ patients = pd.read_csv("patients.csv")
 appointments = pd.read_csv("appointments.csv")
 clinics = pd.read_csv("clinics.csv")
 
-appt_patient = appointments.merge(patients, on="patient_id", how="left")
-df = appt_patient.merge(clinics, on="clinic_id", how="left")
+df = appointments.merge(patients, on="patient_id", how="left")
+df = df.merge(clinics, on="clinic_id", how="left")
 
 df.head()  
 
 ## Step 02 - Load dataset
-if page == "Introduction 📘":
+if page == "Business Case 📘":
 
-    st.subheader("01 Introduction 📘")
+    st.subheader("Healthcare Clinic Revenue Dashboard")
+
+    st.markdown("""
+    ## 🎯 Business Problem
+
+    Healthcare clinics lose revenue due to:
+    - Patient no-shows
+    - Underutilized appointment slots
+    - Inefficient scheduling
+    - Poor resource allocation
+
+    These inefficiencies lead to hidden financial losses.
+    """)
+
+    st.markdown("""
+    ## Our Solution
+
+    1. Identify revenue loss per clinic  
+    2. Predict no-show rates using Linear Regression  
+    3. Recommend optimized scheduling strategies  
+    """)
 
     st.markdown("##### Data Preview")
     rows = st.slider("Select a number of rows to display",5,20,5)
@@ -64,7 +89,7 @@ if page == "Introduction 📘":
     st.write("Appointments:", appointments.shape)
     st.write("Clinics:", clinics.shape)
 
-    if missing_patients.sum() == 0 or missing_appointments == 0 or missing_clinics == 0:
+    if missing_patients.sum() == 0 and missing_appointments.sum() == 0 and missing_clinics.sum() == 0:
         st.success("✅ No missing values found")
     else:
         st.warning("⚠️ you have missing values")
@@ -78,31 +103,67 @@ if page == "Introduction 📘":
 elif page == "Visualization 📊":
 
     ## Step 03 - Data Viz
-    st.subheader("02 Data Viz")
+    st.subheader("02 Data Vizualization")
 
-    col_x = st.selectbox("Select X-axis variable", df.columns, index=0)
-    col_y = st.selectbox("Select Y-axis variable", df.columns, index=1)
+    # col_x = st.selectbox("Select X-axis variable", df.columns, index=0)
+    # col_y = st.selectbox("Select Y-axis variable", df.columns, index=1)
 
-    tab1, tab2, tab3 = st.tabs(["Bar Chart 📊","Line Chart 📈","Correlation Heatmap 🔥"])
+    tab1, tab2, tab3, tab4 = st.tabs(["Revenue Leakage 📊", "Patient Lifetime Value 💰", "Correlation Matrix 🔥" ,"No-Show Rate by Clinic"])
 
     with tab1:
-        st.subheader("Bar Chart")
-        st.bar_chart(df[[col_x,col_y]].sort_values(by=col_x),use_container_width=True)
+        st.subheader("Revenue Leakage Bar Chart")
+        df['revenue_loss'] = df['revenue_expected'] - df['revenue_realized']
+        revenue_by_clinic = df.groupby("clinic_location").agg(
+            expected_revenue=('revenue_expected', 'sum'),
+            realized_revenue=('revenue_realized', 'sum'),
+            revenue_loss=('revenue_loss', 'sum')
+        ).reset_index()
+        revenue_by_clinic['revenue_loss_percent'] = (
+            revenue_by_clinic['revenue_loss'] / revenue_by_clinic['expected_revenue'] * 100
+        )
+        fig1, ax1 = plt.subplots(figsize=(10, 5))
+        sns.barplot(data=revenue_by_clinic, x='clinic_location', y='revenue_loss_percent', ax=ax1)
+        ax1.set_title("Revenue Loss % by Clinic", fontsize=16)
+        ax1.set_ylabel("% Revenue Lost")
+        ax1.set_xlabel("Clinic")
+        st.pyplot(fig1)
 
     with tab2:
-        st.subheader("Line Chart")
-        st.line_chart(df[[col_x,col_y]].sort_values(by=col_x),use_container_width=True)
+        st.subheader("Patient Lifetime Value")
+        ltv = patients.copy()
+        ltv['expected_future_visits'] = 10 - ltv['total_lifetime_visits']
+        ltv['estimated_LTV'] = (
+            ltv['total_lifetime_revenue']
+            + (ltv['expected_future_visits'] * ltv['total_lifetime_revenue']
+            / ltv['total_lifetime_visits'].replace(0, 1))
+        )
+        st.dataframe(ltv.head())
 
 
     with tab3:
         st.subheader("Correlation Matrix")
-        df_numeric = df.select_dtypes(include=np.number)
+        fig3, ax3 = plt.subplots(figsize=(10, 6))
+        sns.heatmap(
+            df[['age', 'chronic_condition_flag', 'total_lifetime_visits',
+                'revenue_expected', 'revenue_realized', 'no_show_flag']].corr(),
+            annot=True,
+            cmap='Blues',
+            fmt=".2f",
+            ax=ax3
+        )
+        ax3.set_title("Correlation Heatmap")
+        st.pyplot(fig3)
 
-        fig_corr, ax_corr = plt.subplots(figsize=(18,14))
-        # create the plot, in this case with seaborn 
-        sns.heatmap(df_numeric.corr(),annot=True,fmt=".2f",cmap='coolwarm')
-        ## render the plot in streamlit 
-        st.pyplot(fig_corr)
+    with tab4:
+        st.subheader("No-Show Rate by Clinic")
+        no_show_by_clinic = df.groupby("clinic_location")["no_show_flag"].mean().reset_index()
+        no_show_by_clinic.rename(columns={"no_show_flag": "no_show_rate"}, inplace=True)
+        fig4, ax4 = plt.subplots(figsize=(10, 5))
+        sns.barplot(data=no_show_by_clinic, x='clinic_location', y='no_show_rate', ax=ax4)
+        ax4.set_title("No-Show Rate by Clinic", fontsize=16)
+        ax4.set_ylabel("No-show Rate")
+        ax4.set_xlabel("Clinic")
+        st.pyplot(fig4)
 
 elif page == "Automated Report 📑":
     st.subheader("03 Automated Report")
@@ -124,7 +185,7 @@ elif page == "Prediction":
     df2 = df2.dropna()
 
     ### Label Encoder to change text categories into number categories
-    from sklearn.preprocessing import LabelEncoder
+    
     le = LabelEncoder()
 
     df2["ocean_proximity"] = le.fit_transform(df2["ocean_proximity"])
@@ -143,14 +204,12 @@ elif page == "Prediction":
     st.dataframe(y.head())
 
     ### ii) train_test_split
-    from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 
 
     ## Model 
 
     ### i) Definition model
-    from sklearn.linear_model import LinearRegression
     model = LinearRegression()
 
     ### ii) Training model
@@ -159,8 +218,7 @@ elif page == "Prediction":
     ### iii) Prediction
     predictions = model.predict(X_test)
 
-    ### iv) Evaluation 
-    from sklearn import metrics 
+    ### iv) Evaluation  
     if "Mean Squared Error (MSE)" in selected_metrics:
         mse = metrics.mean_squared_error(y_test, predictions)
         st.write(f"- **MSE** {mse:,.2f}")
@@ -178,6 +236,6 @@ elif page == "Prediction":
     ax.plot([y_test.min(),y_test.max()],
            [y_test.min(),y_test.max() ],"--r",linewidth=2)
     ax.set_xlabel("Actual Value")
-    ax.set_xlabel("Predicted Value")
+    ax.set_ylabel("Predicted Value")
     ax.set_title("Actual vs Predicted")
     st.pyplot(fig)
