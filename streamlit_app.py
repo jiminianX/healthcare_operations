484820359fc38414f41dcbbe79f7641cca350f26
Joseph Jiminian <jj3945@nyu.edu>
2026-02-25 18:36:36 +0100

    added changes to  automated report

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.set_page_config(
 
 ## Step 01 - Setup
 st.sidebar.title("Healthcare Clinics 🏥")
-page = st.sidebar.selectbox("Select Page",["Business Case 📘","Visualization 📊", "Automated Report 📄", "Prediction"])
+page = st.sidebar.selectbox("Select Page",["Business Case 📘","Visualization 📊", "Automated Report 📑", "Prediction"])
 
 
 #st.video("video.mp4")
@@ -170,7 +170,7 @@ elif page == "Automated Report 📑":
     if st.button("Generate Report"):
         with st.spinner("Generating report..."):
             profile = ProfileReport(df,title="Clinic Revenue Report",explorative=True,minimal=True)
-            html(profile.to_html(), height=1000)
+            html(profile.to_html(), height=1400)
 
         export = profile.to_html()
         st.download_button(label="📥 Download full Report",data=export,file_name="clinic_revenue_report.html",mime='text/html')
